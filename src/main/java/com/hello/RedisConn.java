f95d81a36cddd9cbe1043586eedceaaf1b4197c4
Michael4J <874382676@qq.com>
2018-07-16 14:05:24 +0800

    test RedisConn

--- src/main/java/com/hello/RedisConn.java
+++ src/main/java/com/hello/RedisConn.java
@@ -1,4 +1,16 @@
 package com.hello;
 
+import redis.clients.jedis.Jedis;
+
+/**
+ * RedisConn redis连接
+ */
 public class RedisConn {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.43.128", 6379);
+        jedis.set("key", "hello");
+        System.out.println(jedis.get("key"));
+        jedis.del("key");
+        System.out.println(jedis.get("key"));
+    }
 }
